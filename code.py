1240e531bb7982a7d36882f6ecacc54e0c9b0ed9
Alex Cavnar <alc6379@gmail.com>
2021-03-04 12:34:22 -0600

    updated code.py to new adafruit hid library syntax

--- code.py
+++ code.py
@@ -1,10 +1,12 @@
 import time
+import usb_hid
 from adafruit_hid.mouse import Mouse
  
-mouse = Mouse()
+mouse = Mouse(usb_hid.devices)
 
 while True: 
     mouse.move(x=20)
     time.sleep(2)
     mouse.move(x=-20)
     time.sleep(5)
+    print("jiggle")
